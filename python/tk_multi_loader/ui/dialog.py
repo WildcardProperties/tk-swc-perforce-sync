b27eb44e1f1acd0d3aaf1d0d2af3f7ce7e885a7a
Alaa Ibrahim <alaa@studiowildcard.com>
2023-10-09 09:51:23 -0700

    Update to multiple rows selection in Column view

--- python/tk_multi_loader/ui/dialog.py
+++ python/tk_multi_loader/ui/dialog.py
@@ -288,12 +288,11 @@ class Ui_Dialog(object):
         self.horizontalLayout_7.addWidget(self.publish_view)
 
         self.column_view = QtWidgets.QTableView(self.publish_frame)
-        # Make the column view single selection
-        # self.column_view.setSelectionMode(QtWidgets.QAbstractItemView.SingleSelection)
+
         # Set the selection behavior to select whole rows
         self.column_view.setSelectionBehavior(QtWidgets.QAbstractItemView.SelectRows)
         # Set the selection mode to single selection or multi-selection
-        self.column_view.setSelectionMode(QtWidgets.QAbstractItemView.MultiSelection)
+        #self.column_view.setSelectionMode(QtWidgets.QAbstractItemView.MultiSelection)
 
         self.horizontalLayout_7.addWidget(self.column_view)
         self.column_view.setVisible(False)
